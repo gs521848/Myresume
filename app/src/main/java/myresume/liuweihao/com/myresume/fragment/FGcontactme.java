2b813e98cf507daaad5ee53741810deec89b233f
gs521848 <527930234@qq.com>
2019-01-01 23:29:16 +0800

    简历初始化 tapview

---
+++ app/src/main/java/myresume/liuweihao/com/myresume/fragment/FGcontactme.java
@@ -0,0 +1,12 @@
+package myresume.liuweihao.com.myresume.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2019/1/1.
+ */
+
+public class FGcontactme extends Fragment {
+
+}
---
+++ app/src/main/java/myresume/liuweihao/com/myresume/fragment/FGresume.java
@@ -0,0 +1,11 @@
+package myresume.liuweihao.com.myresume.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Administrator on 2019/1/1.
+ */
+
+public class FGresume extends Fragment {
+}
